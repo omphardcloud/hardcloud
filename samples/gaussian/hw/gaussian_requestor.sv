1d19c0284aa8746275f50d66709eb382c4364bde
Ciro Ceissler <ciro.ceissler@gmail.com>
2017-12-12 21:51:21 -0200

    guassian - change from physical to virtual address

--- samples/gaussian/hw/gaussian_requestor.sv
+++ samples/gaussian/hw/gaussian_requestor.sv
@@ -8,7 +8,7 @@ module gaussian_requestor
   input  logic           clk,
   input  logic           reset,
   input  logic [31:0]    hc_control,
-  input  t_ccip_clAddr   hc_dsm_base,
+  input  t_hc_address    hc_dsm_base,
   input  t_hc_buffer     hc_buffer[HC_BUFFER_SIZE],
   input  logic [511:0]   data_in,
   input  logic           valid_in,
@@ -176,7 +176,7 @@ module gaussian_requestor
       S_WR_FINISH_1:
         begin
           if (!ccip_rx.c1TxAlmFull && (wr_rsp_cnt == hc_buffer[0].size - 2)) begin
-            wr_hdr.address = hc_dsm_base + 1;
+            wr_hdr.address = hc_dsm_base;
             wr_hdr.sop = 1'b1;
 
             ccip_c1_tx.hdr   <= wr_hdr;
