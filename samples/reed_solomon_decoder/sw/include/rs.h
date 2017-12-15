571c415038da5758eec7c89f26799b2c81131a2d
Ciro Ceissler <ciro.ceissler@gmail.com>
2017-12-14 23:59:02 -0200

    rs decoder: add rs files

---
+++ samples/reed_solomon_decoder/sw/include/rs.h
@@ -0,0 +1,7 @@
+#ifndef RS_H_
+#define RS_H_
+
+void rsenc_204 (unsigned char* data_out, unsigned char* data_in);
+void rsdec_204(unsigned char* data_out, unsigned char* data_in);
+
+#endif // RS_H_
