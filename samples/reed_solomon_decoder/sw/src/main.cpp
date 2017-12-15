07371ff04ba5d755a517f8d69b2a50bba055991a
Ciro Ceissler <ciro.ceissler@gmail.com>
2017-12-14 23:55:47 -0200

    rs decoder: initial sw version

--- samples/reed_solomon_decoder/sw/src/main.cpp
+++ samples/reed_solomon_decoder/sw/src/main.cpp
@@ -3,37 +3,48 @@
 #include <stdint.h>
 
 #include "harp.h"
+#include "rs.h"
 
 #define CL 64 // cache line - bytes
 
-#define NI 52*CL/sizeof(uint32_t) // number of itens
-#define NJ 47*CL/sizeof(uint32_t) // number of itens
+#ifdef DEBUG
+  #define NI 52*CL/sizeof(uint8_t) // number of itens
+  #define NJ 47*CL/sizeof(uint8_t) // number of itens
+#else
+  #define NI 20400000*CL/sizeof(uint8_t) // number of itens
+  #define NJ 18800000*CL/sizeof(uint8_t) // number of itens
+#endif // DEBUG
 
 int main()
 {
-  uint32_t* data_in;
-  uint32_t* data_out;
+  uint8_t* data_in;
+  uint8_t* data_out;
 
-  data_in  = (uint32_t *) malloc (NI*sizeof(uint32_t));
-  data_out = (uint32_t *) malloc (NJ*sizeof(uint32_t));
+  data_in  = (uint8_t *) malloc (NI*sizeof(uint8_t));
+  data_out = (uint8_t *) malloc (NJ*sizeof(uint8_t));
 
-  for (uint32_t i = 0; i < NI; i++)
+  for (uint64_t i = 0; i < NI; i++)
   {
     data_in[i] = rand();
   }
 
   #pragma omp target device(HARPSIM) map(to: data_in[:NI]) map(from: data_out[:NJ])
-  #pragma omp parallel use(hrw) module(reed_solomon_decoder)
+  #pragma omp parallel for use(hrw) module(reed_solomon_decoder)
+  for (uint64_t i = 0; i < NI/204; i++)
   {
-    data_out[0] = data_in[0];
+    data_out[i*188] = data_in[i*204];
+
+    rsdec_204((unsigned char*) data_out + i*188, (unsigned char*) data_in + i*204);
   }
 
-  for (uint32_t i = 0; i < NJ; i ++)
+#ifdef DEBUG
+  for (uint64_t i = 0; i < NJ; i ++)
   {
-    printf("data_out[%u] = %08x\n", i, data_out[i]);
+    printf("data_out[%u] = %02x\n", i, data_out[i]);
   }
 
   printf("\n");
+#endif // DEBUG
 
   free(data_in);
   free(data_out);
