720008f240a6d9eec516d401fc5d9eb841a67b0a
ciroceissler <ciro.ceissler@gmail.com>
2019-09-09 20:53:47 -0300

    remove some samples because of redundancy!

--- samples/gaussian_aws/sw/include/awsf1.h
+++
@@ -1,6 +0,0 @@
-#ifndef __AWSF1_H__
-#define __AWSF1_H__
-
-#define AWSF1 9007
-
-#endif // __HARP_H__
--- samples/loopback_aws/sw/include/awsf1.h
+++
@@ -1,6 +0,0 @@
-#ifndef __AWSF1_H__
-#define __AWSF1_H__
-
-#define AWSF1 9007
-
-#endif // __AWSF1_H__
