9f9a76fd818003d995588a0903222b73aaf29c7a
ciroceissler <ciro.ceissler@gmail.com>
2019-12-08 16:24:26 -0300

    hardcloud script add target arg

--- scripts/sdx/hardcloud.py
+++ scripts/sdx/hardcloud.py
@@ -6,8 +6,8 @@ import argparse
 
 class Hardcloud:
 
-  def __init__(self, config):
-    self.config    = config
+  def __init__(self, target):
+    self.target    = target
     self.base_path = os.path.abspath(os.path.dirname(__file__))
     self.project   = os.getcwd().split('/')[-1]
 
@@ -36,7 +36,12 @@ class Hardcloud:
 
     os.system('vivado -mode tcl -source rtl_kernel/generate.tcl')
 
-    os.system('xocc -t hw_emu \
+    target = 'hw_emu'
+
+    if self.target == "FPGA":
+      target = 'hw'
+
+    os.system('xocc -t ' + target + ' \
         --platform xilinx_u200_xdma_201830_2 \
         --sp hardcloud_top_1.m00_axi:DDR[0] \
         --sp hardcloud_top_1.m01_axi:DDR[1] \
@@ -58,7 +63,7 @@ if __name__ == '__main__':
 
   parser.add_argument('--new_project', help='create new project')
   parser.add_argument('--build'      , action='store_true', help='build system')
-  parser.add_argument('--config'     , choices=['system', 'emulation_hw'], help='set configuration')
+  parser.add_argument('--target'     , choices=['FPGA', 'emulation'], help='set configuration')
 
   args = parser.parse_args()
 
@@ -66,7 +71,7 @@ if __name__ == '__main__':
     parser.print_help()
     parser.exit()
 
-  obj = Hardcloud(args.config)
+  obj = Hardcloud(args.target)
 
   if args.new_project is not None:
     obj.new_project(args.new_project)
